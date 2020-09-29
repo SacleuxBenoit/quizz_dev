afb0e9ecb9601ae7d7dbade44e14a8c2322e77cd
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-29 13:38:34 +0200

    add 4div, with 4 buttons, for HTML, CSS, JS and PHP in index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Quizz dev</h1>
+
+    <div class="divHTML">
+        <button value="HTML" class="HTML">HTML</button>
+    </div>
+
+    <div class="divCSS">
+        <button value="CSS" class="CSS">CSS</button>
+    </div>
+
+    <div class="divJS">
+        <button value="JS" class="JS">JS</button>
+    </div>
+
+    <div class="divPHP">
+        <button value="PHP" class="PHP">PHP</button>
+    </div>
+</body>
+</html>
