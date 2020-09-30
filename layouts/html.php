5bdd3433b5e97014a3e08a181f100b6f8832a8cc
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-30 17:36:56 +0200

    change input type=radio, to type=checkbox in layouts/html.php

--- layouts/html.php
+++ layouts/html.php
@@ -12,32 +12,37 @@
     <?php
         include('../database/connection_database.php');
 
-        $question = $bdd->query('SELECT * FROM HTML ORDER BY RAND() LIMIT 2');
+        $question = $bdd->query('SELECT * FROM HTML ORDER BY RAND() LIMIT 1');
 
         while($get_question = $question->fetch()){
             ?>
-                <form action="html.php" method="post">
+                
                     <div>
                         <?php echo $get_question['question']?>
 
                         <p>
-                            <input type="radio" name="answer" id="firstAnswer">
+                            <input type="checkbox" id="firstAnswer" name="answer">
                             <label for="firstAnswer"><?php echo $get_question['first_answer'] ?></label>
                         </p>
 
                         <p>
-                            <input type="radio" name="answer" id="secondAnswer">
+                            <input type="checkbox" id="secondAnswer" name="answer">
                             <label for="secondAnswer"><?php echo $get_question['second_answer'] ?></label>
                         </p>
                         
                         <p>
-                            <input type="radio" name="answer" id="thirdAnswer">
+                            <input type="checkbox" id="thirdAnswer" name="answer">
                             <label for="thirdAnswer"><?php echo $get_question['third_answer'] ?></label>
                         </p>
+
+                            <input type="Submit" value="Submit">
+
                     </div>
-                </form>
+
+              
             <?php
         }
-    ?>
+            ?>
+
 </body>
 </html>
